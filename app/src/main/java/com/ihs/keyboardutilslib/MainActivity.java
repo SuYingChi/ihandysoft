bc4f0cf52df4efea5cd771bff86fbfdb5d6bb327
weiqianwang <weiqian.wang@ihandysoft.com>
2016-10-21 15:50:41 +0800

    add panel module

--- app/src/main/java/com/ihs/keyboardutilslib/MainActivity.java
+++ app/src/main/java/com/ihs/keyboardutilslib/MainActivity.java
@@ -1,7 +1,11 @@
 package com.ihs.keyboardutilslib;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import com.ihs.keyboardutilslib.PanelContainer.PanelContainerActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void startPanelContainer(View view) {
+        startActivity(new Intent(this, PanelContainerActivity.class));
     }
 }
---
+++ app/src/main/java/com/ihs/keyboardutilslib/PanelContainer/PanelContainerActivity.java
@@ -0,0 +1,18 @@
+package com.ihs.keyboardutilslib.PanelContainer;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.ihs.keyboardutilslib.R;
+
+/**
+ * Created by Arthur on 16/10/21.
+ */
+
+public class PanelContainerActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_panel_container);
+    }
+}
