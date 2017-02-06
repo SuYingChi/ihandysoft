17ce1a39cbf51b95cba2149e15aecf057e20fdfb
zhendong.wang <zhendong.wang@ihandysoft.com>
2017-02-06 13:12:25 +0800

    Add round corner for gif image view

--- keyboardutils/src/main/java/com/ihs/keyboardutils/view/HSGifImageView.java
+++ keyboardutils/src/main/java/com/ihs/keyboardutils/view/HSGifImageView.java
@@ -1,12 +1,22 @@
 package com.ihs.keyboardutils.view;
 
 import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
+import android.graphics.RectF;
 import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.util.AttributeSet;
 import android.view.ViewGroup;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
+
+import com.ihs.keyboardutils.R;
 
 import pl.droidsonroids.gif.GifDrawable;
 import pl.droidsonroids.gif.GifImageView;
@@ -17,6 +27,11 @@ import pl.droidsonroids.gif.GifImageView;
 
 public class HSGifImageView extends FrameLayout {
     private GifImageView mGifImageView;
+
+    // Draw round corner
+    private Paint imagePaint;
+    private Paint roundPaint;
+    private float cornerRadius = 0;
 
     public HSGifImageView(Context context) {
         super(context);
@@ -36,6 +51,28 @@ public class HSGifImageView extends FrameLayout {
     private void initView(AttributeSet attrs) {
         mGifImageView = new GifImageView(getContext(),attrs);
         addView(mGifImageView,new LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+
+        // Get corner cornerRadius
+        if (attrs != null) {
+            TypedArray ta = getContext().obtainStyledAttributes(attrs, R.styleable.GifImageView);
+
+            if (ta != null) {
+                cornerRadius = ta.getDimension(R.styleable.GifImageView_corner_radius, 0);
+                ta.recycle();
+            }
+        }
+
+        // Draw round corner
+        if (cornerRadius > 0) {
+            roundPaint = new Paint();
+            roundPaint.setColor(Color.WHITE);
+            roundPaint.setAntiAlias(true);
+            roundPaint.setStyle(Paint.Style.FILL);
+            roundPaint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.DST_OUT));
+
+            imagePaint = new Paint();
+            imagePaint.setXfermode(null);
+        }
     }
 
     public void setImageURI(Uri uri){
@@ -89,4 +126,65 @@ public class HSGifImageView extends FrameLayout {
         return null;
     }
 
+    @Override
+    protected void dispatchDraw(Canvas canvas) {
+        if (cornerRadius > 0) {
+            canvas.saveLayer(new RectF(0, 0, canvas.getWidth(), canvas.getHeight()), imagePaint, Canvas.ALL_SAVE_FLAG);
+            super.dispatchDraw(canvas);
+
+            // Draw round corner
+            drawTopLeft(canvas);
+            drawTopRight(canvas);
+            drawBottomLeft(canvas);
+            drawBottomRight(canvas);
+
+            canvas.restore();
+        } else {
+            super.dispatchDraw(canvas);
+        }
+    }
+
+    private void drawTopLeft(Canvas canvas) {
+        Path path = new Path();
+        path.moveTo(0, cornerRadius);
+        path.lineTo(0, 0);
+        path.lineTo(cornerRadius, 0);
+        path.arcTo(new RectF(0, 0, cornerRadius * 2, cornerRadius * 2), -90, -90);
+        path.close();
+        canvas.drawPath(path, roundPaint);
+    }
+
+    private void drawTopRight(Canvas canvas) {
+        int width = getWidth();
+        Path path = new Path();
+        path.moveTo(width - cornerRadius, 0);
+        path.lineTo(width, 0);
+        path.lineTo(width, cornerRadius);
+        path.arcTo(new RectF(width - 2 * cornerRadius, 0, width, cornerRadius * 2), 0, -90);
+        path.close();
+        canvas.drawPath(path, roundPaint);
+    }
+
+    private void drawBottomLeft(Canvas canvas) {
+        int height = getHeight();
+        Path path = new Path();
+        path.moveTo(0, height - cornerRadius);
+        path.lineTo(0, height);
+        path.lineTo(cornerRadius, height);
+        path.arcTo(new RectF(0, height - 2 * cornerRadius, cornerRadius * 2, height), 90, 90);
+        path.close();
+        canvas.drawPath(path, roundPaint);
+    }
+
+    private void drawBottomRight(Canvas canvas) {
+        int height = getHeight();
+        int width = getWidth();
+        Path path = new Path();
+        path.moveTo(width - cornerRadius, height);
+        path.lineTo(width, height);
+        path.lineTo(width, height - cornerRadius);
+        path.arcTo(new RectF(width - 2 * cornerRadius, height - 2 * cornerRadius, width, height), 0, 90);
+        path.close();
+        canvas.drawPath(path, roundPaint);
+    }
 }
