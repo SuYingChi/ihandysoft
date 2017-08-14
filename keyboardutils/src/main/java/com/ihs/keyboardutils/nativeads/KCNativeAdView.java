c6a3752bad20f1716a77e89b82aea0930e0ca234
rongbo.qi <rongbo.qi@ihandysoft.com>
2017-08-14 16:51:30 +0800

    暂时禁用原生广告的角标

--- keyboardutils/src/main/java/com/ihs/keyboardutils/nativeads/KCNativeAdView.java
+++ keyboardutils/src/main/java/com/ihs/keyboardutils/nativeads/KCNativeAdView.java
@@ -112,6 +112,8 @@ public class KCNativeAdView extends FrameLayout {
         this.nativeAdContainerView = new AcbNativeAdContainerView(getContext());
         this.nativeAdContainerView.addContentView(groupView);
 
+        this.nativeAdContainerView.hideAdCorner();
+
         int coverImgId = getResources().getIdentifier("ad_cover_img", "id", getContext().getPackageName());
         int choiceId = getResources().getIdentifier("ad_choice", "id", getContext().getPackageName());
         int actionId = getResources().getIdentifier("ad_call_to_action", "id", getContext().getPackageName());
