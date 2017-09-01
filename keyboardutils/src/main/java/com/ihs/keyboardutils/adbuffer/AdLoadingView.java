4baf801fd868ede71570b1cca00087abf1f07285
yangliu <yang.liu@ihandysoft.com>
2017-09-01 15:32:07 +0800

    review修改

--- keyboardutils/src/main/java/com/ihs/keyboardutils/adbuffer/AdLoadingView.java
+++ keyboardutils/src/main/java/com/ihs/keyboardutils/adbuffer/AdLoadingView.java
@@ -88,7 +88,7 @@ public class AdLoadingView extends RelativeLayout implements KCNativeAdView.OnAd
         tvApply = (TextView) findViewById(R.id.tv_apply);
 
         LinearLayout rootView = (LinearLayout) findViewById(R.id.root_view);
-        rootView.getLayoutParams().height = (int)(((Activity) getContext()).getWindowManager().getDefaultDisplay().getWidth() * 1.2);
+        rootView.getLayoutParams().height = (int)(((Activity) getContext()).getWindowManager().getDefaultDisplay().getHeight() * 0.8);
 
         FlashFrameLayout sponsoredContent = (FlashFrameLayout) findViewById(R.id.sponsored_content);
         sponsoredContent.setDuration(3000);
