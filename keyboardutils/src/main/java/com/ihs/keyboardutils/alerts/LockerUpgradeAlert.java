09fb4820bd94e82cbf8f33a7efb7b140156901f4
yangliu <yang.liu@ihandysoft.com>
2017-12-12 17:44:45 +0800

    UI微调

--- keyboardutils/src/main/java/com/ihs/keyboardutils/alerts/LockerUpgradeAlert.java
+++ keyboardutils/src/main/java/com/ihs/keyboardutils/alerts/LockerUpgradeAlert.java
@@ -1,6 +1,7 @@
 package com.ihs.keyboardutils.alerts;
 
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AlertDialog;
@@ -10,7 +11,6 @@ import android.widget.FrameLayout;
 import android.widget.ImageView;
 
 import com.artw.lockscreen.lockerappguide.LockerAppGuideManager;
-import com.ihs.chargingscreen.utils.DisplayUtils;
 import com.ihs.chargingscreen.utils.LockerChargingSpecialConfig;
 import com.ihs.keyboardutils.R;
 
@@ -21,7 +21,7 @@ import com.ihs.keyboardutils.R;
 public class LockerUpgradeAlert extends AlertDialog implements View.OnClickListener {
 
     public LockerUpgradeAlert(@NonNull Context context) {
-        super(context);
+        super(context, R.style.AlertDialogTheme);
     }
 
     @Override
@@ -30,13 +30,10 @@ public class LockerUpgradeAlert extends AlertDialog implements View.OnClickListe
         setContentView(R.layout.locker_upgrade_alert);
         FrameLayout rootView = findViewById(R.id.root_view);
 
-        int screenWidth = DisplayUtils.getScreenWidthPixels();
-        rootView.getLayoutParams().width = (int) getContext().getResources().getFraction(R.fraction.design_dialog_width, screenWidth, screenWidth);
-
         Button upgradeButton = rootView.findViewById(R.id.upgrade_button);
-        upgradeButton.setOnClickListener(view -> {
-            LockerAppGuideManager.directToMarket(null, null, LockerChargingSpecialConfig.BOUND_SERVICE_PACKAGE);
-        });
+        upgradeButton.setOnClickListener(view -> LockerAppGuideManager.directToMarket(null, null, LockerChargingSpecialConfig.BOUND_SERVICE_PACKAGE));
+
+        setCancelable(false);
 
         ImageView closeIcon = rootView.findViewById(R.id.close_icon);
         closeIcon.setOnClickListener(view -> dismiss());
