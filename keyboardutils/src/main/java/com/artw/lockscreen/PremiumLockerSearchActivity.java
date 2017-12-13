2808f8aa986f923684270cf8fd3eb17720249289
yanxia <yan.xia@ihandysoft.com>
2017-12-13 17:27:25 +0800

    add animation

--- keyboardutils/src/main/java/com/artw/lockscreen/PremiumLockerSearchActivity.java
+++ keyboardutils/src/main/java/com/artw/lockscreen/PremiumLockerSearchActivity.java
@@ -4,12 +4,16 @@ import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
 
 import com.artw.lockscreen.common.LockerChargingScreenUtils;
 import com.ihs.commons.utils.HSLog;
 import com.ihs.keyboardutils.R;
+import com.ihs.keyboardutils.utils.LauncherAnimationUtils;
 import com.ihs.keyboardutils.view.SearchEditTextView;
 
 /**
@@ -19,6 +23,8 @@ import com.ihs.keyboardutils.view.SearchEditTextView;
 public class PremiumLockerSearchActivity extends AppCompatActivity {
 
     private SearchEditTextView searchEditTextView;
+    private boolean animationPlayed;
+    private AlphaAnimation alphaAnimation;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -44,5 +50,57 @@ public class PremiumLockerSearchActivity extends AppCompatActivity {
                 HSLog.d(searchText);
             }
         });
+    }
+
+    /**
+     * Dispatch onResume() to fragments.  Note that for better inter-operation
+     * with older versions of the platform, at the point of this call the
+     * fragments attached to the activity are <em>not</em> resumed.  This means
+     * that in some cases the previous state may still be saved, not allowing
+     * fragment transactions that modify the state.  To correctly interact
+     * with fragments in their proper state, you should instead override
+     * {@link #onResumeFragments()}.
+     */
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (!animationPlayed) {
+            doAnimation();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (alphaAnimation != null) {
+            alphaAnimation.cancel();
+        }
+    }
+
+    private void doAnimation() {
+        if (alphaAnimation == null) {
+            alphaAnimation = new AlphaAnimation(0.0f, 1.0f);
+            alphaAnimation.setDuration(500);
+            alphaAnimation.setAnimationListener(new LauncherAnimationUtils.AnimationListenerAdapter());
+            alphaAnimation.setFillAfter(true);
+            alphaAnimation.setAnimationListener(new Animation.AnimationListener() {
+                @Override
+                public void onAnimationStart(Animation animation) {
+                    searchEditTextView.setVisibility(View.VISIBLE);
+                    animationPlayed = true;
+                }
+
+                @Override
+                public void onAnimationEnd(Animation animation) {
+
+                }
+
+                @Override
+                public void onAnimationRepeat(Animation animation) {
+
+                }
+            });
+        }
+        searchEditTextView.startAnimation(alphaAnimation);
     }
 }
