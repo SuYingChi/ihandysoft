2c8b8661c52ff933a556f1155904f441802fdef8
rongbo.qi <rongbo.qi@ihandysoft.com>
2018-01-16 14:45:33 +0800

    add map parameters support for analytics

--- keyboardutils/src/main/java/com/kc/utils/KCAnalytics.java
+++ keyboardutils/src/main/java/com/kc/utils/KCAnalytics.java
@@ -4,11 +4,23 @@ import com.crashlytics.android.answers.Answers;
 import com.crashlytics.android.answers.CustomEvent;
 import com.ihs.app.analytics.HSAnalytics;
 
+import java.util.Map;
+
 public class KCAnalytics {
     public static void logEvent(String eventID, String... vars) {
         logFabricEvent(eventID, vars);
 
         logFlurryEvent(eventID, vars);
+    }
+
+    public static void logEvent(String eventID, Map<String, String> params) {
+        String[] paramArray = new String[params.size() * 2];
+        int index = 0;
+        for (Map.Entry<String, String> entry : params.entrySet()) {
+            paramArray[index ++] = entry.getKey();
+            paramArray[index ++] = entry.getValue();
+        }
+        logEvent(eventID, paramArray);
     }
 
     public static void logFlurryEvent(String eventID, String... vars) {
